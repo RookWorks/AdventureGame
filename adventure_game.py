ec8717e81aef1f681f0fe675640f66a5c13ad01a
Cam <64113264+RookWorks@users.noreply.github.com>
2022-05-30 11:06:43 -0400

    added some more random options for the computer to load

--- adventure_game.py
+++ adventure_game.py
@@ -7,45 +7,46 @@ def print_pause(msg_to_print):
     time.sleep(2)
 
 
-def intro(items, option):
+def intro(items, option, homes, questGiver, arenas):
     print_pause("You awaken in a field of flowers, with cold looking "
-                "mountains to your right and a cozy cabin to your left.\n")
+                + arenas + " to your right and a " + homes + 
+                " to your left.\n")
     print_pause("You suddenly remember your quest! You need to "
-                "retrieve " + option + "'s tooth for a witch that "
-                "lives near by.\n")
+                "retrieve " + option + "'s tooth for a " + questGiver + 
+                " that lives near by.\n")
     print_pause("In your hand is nothing but a strange tingling sensation, "
                 "a strange thrum beneath the skin. Maybe you should ask the "
-                "witch about that...\n")
+                + questGiver + " about that...\n")
 
 
-def cabin(items, option):
-    print_pause("You approach the cabin of the witch and knock on the door.\n")
+def home(items, option, homes, questGiver, arenas):
+    print_pause("You approach the " + homes + " of the " + questGiver + " and knock on the door.\n")
     if "tooth" in items:
-        print_pause("The witch opens the door to the cabin, her face set in "
-                    "a scowl, which suddenly brightens up when you show her "
+        print_pause("The " + questGiver + " opens the door to the " + homes + ", their face set in "
+                    "a scowl, which suddenly brightens up when you show their "
                     "what you have. \n")
         print_pause("\"Ah! The tooth of a " + option + "!\" \n")
         print_pause("\"Just what I need to finish my spell! Thank you my "
                     "dear! Here's your reward.\" \n")
-        print_pause("The witch smiles as she hands you two pennies...\n")
+        print_pause("The " + questGiver + " smiles as they hand you two pennies...\n")
         print_pause("Maybe this adventuring life isn't what it's cracked up"
                     " to be...\n")
         play_again()
     else:
         print_pause("\"Well? Did you get it? What do you mean your hand is"
-                    " tingling you-!\" \n "
-                    "\"Oh just give me your hand!-\" \n")
-        print_pause("\"Oh! Hmm, well my dear it just seems like you need a"
+                    " tingling you-!\" \n ")
+        print_pause("\"Oh just give me your hand!-\" \n")
+        print_pause("\"Oh! Hmm, well it just seems like you need a"
                     "little magic...\" \n")
-        print_pause("Her hand runs over yours as she giggles and there's a "
+        print_pause("They hand runs over yours as they giggles and there's a "
                     "faint glow there now. Like a weapon primed for use...\n")
         items.append("magic")
     print_pause("You head back the way you came.")
-    adventure(items, option)
+    adventure(items, option, homes, questGiver, arenas)
 
 
-def mountains(items, option):
-    print_pause("The trek up the mountains is an arduous one. But you are "
+def arena(items, option, homes, questGiver, arenas):
+    print_pause("The trek up the " + arenas + " is an arduous one. But you are "
                 "stubborn.\n")
     print_pause("Cresting over one last cliff you see the creature you're"
                 " looking for, a " + option + "! And it sees you!\n")
@@ -63,16 +64,16 @@ def mountains(items, option):
                 print_pause("You thrust your hand out unleashing the magic "
                             "given to you on the " + option + "!\n")
                 print_pause("A crack of thunder and force echos through the "
-                            "mountains and sends the creature tumbling end "
+                            + arenas + " and sends the creature tumbling end "
                             "over end before slamming against the rock wall "
                             "behind it!\n")
                 print_pause("With the creature nullified for now, you set to"
-                            " work gathering what the witch had requested.\n")
+                            " work gathering what the " + questGiver + " had requested.\n")
                 print_pause("...")
                 print_pause("You drop the tooth into your pouch and hurry to"
                             " leave before the " + option + " wakes up.\n")
                 items.append("tooth")
-                adventure(items, option)
+                adventure(items, option, homes, questGiver, arenas)
             else:
                 print_pause("You do your best but your bare hands could do"
                             " nothing against the " + option + ".\n")
@@ -85,19 +86,19 @@ def mountains(items, option):
                         "hitting your back!\n")
             print_pause("Luckily, it doesn't appear that you've been "
                         "followed.\n")
-        adventure(items, option)
+        adventure(items, option, homes, questGiver, arenas)
         break
 
 
-def adventure(items, option):
+def adventure(items, option, homes, questGiver, arenas):
     print_pause("You find yourself back in the fields.\n")
     while True:
-        branch1 = input("Where would you like to go?\n")
-        if branch1 == "cabin".lower():
-            cabin(items, option)
+        branch1 = input("Where would you like to go? Left or right?\n")
+        if branch1 == "right":
+            home(items, option, homes, questGiver, arenas)
             break
-        if branch1 == "mountains".lower():
-            mountains(items, option)
+        if branch1 == "left":
+            arena(items, option, homes, questGiver, arenas)
             break
 
 
@@ -112,15 +113,20 @@ def play_again():
     else:
         play_again()
 
-
 def play_game():
     items = []
     option = random.choice(["dragon", "troll", "goblin", "rust monster",
-                            "Manager Bob",
                             "horrifying creature with many mouths",
                             "evil twin"])
-    intro(items, option)
-    adventure(items, option)
+    questGiver = random.choice(["witch", "old man", "wizard", 
+                                "old woman", "mysterious floating orb", 
+                                "cloaked figure"])
+    homes = random.choice(["cabin", "mansion", "temple", "tavern", 
+                            "meadhall", "shack", "hovel"])
+    arenas = random.choice(["mountain cliff", "underground world", 
+                            "sky", "sea", "abyss", "forest", "valhalla"])
+    intro(items, option, homes, questGiver, arenas)
+    adventure(items, option, homes, questGiver, arenas)
 
 
 play_game()
